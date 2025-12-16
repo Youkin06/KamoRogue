14ad0e85a7625e742bcc4acf85212a92c5429c43
Youkin06 <yopi.games.tokyo@gmail.com>
2025-12-16 11:53:51 +0900

    敵の向き、速度修正

--- Enemy1.py
+++ Enemy1.py
@@ -4,7 +4,7 @@ import Enemy
 class Enemy1(Enemy.Enemy):
     def __init__(self, x, y):
         super().__init__(x, y, 10)
-        self.vx = 2
+        self.vx = 1
         self.size = 4
         self.color = 8 
 
@@ -25,18 +25,10 @@ class Enemy1(Enemy.Enemy):
         if self.hp > 0:
             u = (pyxel.frame_count // 6 % 2) * 16
             w = 16
-            if self.vx > 0: # Facing Right? Assuming sprite faces left by default or something? 
-                            # Let's assume sprite faces left like player usually does? 
-                            # Wait, Player code says `if self.facingLeft: w = -16`. 
-                            # This implies default (positive w) is Right facing? 
-                            # Let's look at Player.py again.
-                            # Player.py: u=... w=16. if facingLeft: w=-16. 
-                            # So default w=16 is Right. 
-                            # If Enemy vx > 0 (Right), w should be 16.
-                            # If Enemy vx < 0 (Left), w should be -16.
-                w = 16
-            else:
+            if self.vx > 0:
                 w = -16
+            else:
+                w = 16
             
             pyxel.blt(self.x, self.y, 0, u, 48, w, 16, 0)
            
