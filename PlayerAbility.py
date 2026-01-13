c542d493da68c321f3b7d3426c7cde994a571434
Youkin06 <yopi.games.tokyo@gmail.com>
2026-01-13 11:58:57 +0900

    ハート追加アビリティ追加

--- PlayerAbility.py
+++ PlayerAbility.py
@@ -31,7 +31,7 @@ class PlayerAbility:
     def __init__(self):
         self.jumpMaxCount = 1
         self.CanDash = False
-        self.CanGuard = True
+        self.CanGuard = False
         self.acquired_abilities = []
         
 
